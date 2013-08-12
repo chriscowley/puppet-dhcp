5f2e3327085df8683320a7ff406ca5c43baeaf56
Chris Cowley <chris@chriscowley.me.uk>
2013-08-13 00:51:12 +0400

    Simple module working

---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+# Class dhcp
+
+class dhcp (
+  $ensure        = 'running',
+  $enable        = true,
+  $subnet        = undef,
+  $netmask       = undef,
+  $domainname    = undef,
+  $nameservers   = undef,
+  $addressrange  = undef,
+  $router        = undef,
+) {
+  case $operatingsystem {
+    centos, redhat: {
+       $dhcp_package = 'dhcp'
+    }
+    debian, ubuntu: {
+      $dhcp_package = 'dhcpd'
+    }
+  }
+  $dhcp_service = 'dhcpd'
+  $dhcp_conf    = '/etc/dhcp/dhcpd.conf'
+
+
+  package { $dhcp_package:
+    ensure => latest
+  }
+
+  service { $dhcp_service:
+    ensure => $ensure,
+    enable => $enable,
+    hasstatus => true,
+    require   => Package[$dhcp_package],
+  }
+  
+  file { $dhcp_conf:
+    content => template('dhcp/dhcpd.conf.erb'),
+    require => Package[$dhcp_package],
+    notify  => Service[$dhcp_service],
+  }
+}
